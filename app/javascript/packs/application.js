431e0b8e715c7a12fb1f2b0587b8b5558fd8858c
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-06-12 23:10:41 +0900

    turbolinks無効化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,7 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("turbolinks").start()
+// require("turbolinks").start() Javascriptのイベント発火を確実にするため無効化
 require("@rails/activestorage").start()
 require("channels")
 
