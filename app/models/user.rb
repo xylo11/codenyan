caaf0a094abc7947a837967ba7bb51728b70f10b
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-06-19 02:22:49 +0900

    rubocop自動整形

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :username, presence: { message: 'を入力してください' }
   validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'は半角英数字混合で入力してください' }
 end
