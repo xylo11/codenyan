1915fbce1d4947d684666c538a6fca227fbf9567
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-06-19 02:21:47 +0900

    ユーザー管理テストコード

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    username { Faker::Name.name }
+    email { Faker::Internet.free_email }
+    password { '1a' + Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
   end
 end
