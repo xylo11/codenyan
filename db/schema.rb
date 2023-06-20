b985592ae376c48bd4293f0e883008147900a07f
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-06-20 15:33:51 +0900

    db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_20_061453) do
+ActiveRecord::Schema.define(version: 2023_06_20_062436) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,15 @@ ActiveRecord::Schema.define(version: 2023_06_20_061453) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "park_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "park_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["park_id"], name: "index_park_users_on_park_id"
+    t.index ["user_id"], name: "index_park_users_on_user_id"
   end
 
   create_table "parks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
@@ -63,5 +72,7 @@ ActiveRecord::Schema.define(version: 2023_06_20_061453) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "park_users", "parks"
+  add_foreign_key "park_users", "users"
   add_foreign_key "user_profiles", "users"
 end
