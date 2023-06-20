67438a258567053af91599a90b83237b2db68e27
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-06-20 22:02:25 +0900

    ビューの調整

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ require("@rails/ujs").start()
 // require("turbolinks").start() Javascriptのイベント発火を確実にするため無効化
 require("@rails/activestorage").start()
 require("channels")
-
+//= require jquery
+//= require jquery_ujs
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
