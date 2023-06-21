b9fd30ab0ae148fdc92f89342a042dc18a13635a
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-06-21 19:17:24 +0900

    投稿コントローラー

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,21 @@
 class PostsController < ApplicationController
-  def index
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user = current_user
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :park_id)
   end
 end
