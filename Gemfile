8f3f929cac77086e2b9859fe7a2b918b78dbf805
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-06-24 23:29:26 +0900

    投稿詳細ビューにマークダウン出力

--- Gemfile
+++ Gemfile
@@ -70,3 +70,5 @@ gem 'mini_magick'
 gem 'image_processing', '~> 1.2'
 
 gem 'jquery-rails'
+
+gem 'redcarpet'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def markdown(text)
+    render_options = {
+      filter_html:     true,
+      hard_wrap:       true, 
+      link_attributes: { rel: 'nofollow' }
+    }
+    renderer = Redcarpet::Render::HTML.new(render_options)
+
+    extensions = {
+      autolink:           true,
+      fenced_code_blocks: true
+    }
+
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render text).html_safe
+  end
 end
