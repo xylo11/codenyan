dd9470cbc309621715d8aaf5a4ffa7017393afa5
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-06-26 04:36:39 +0900

    非同期post更新キャンセルボタン

--- app/javascript/packs/post_edit.js
+++ app/javascript/packs/post_edit.js
@@ -5,4 +5,11 @@ $(document).ready(function() {
     postDiv.find(".content-display").hide(); // Hide the content display
     postDiv.find(".edit-form").show(); // Show the edit form
   });
+
+  $(".cancel-edit-form").click(function(e) {
+    e.preventDefault();
+    var postDiv = $(this).closest(".posted-textarea");
+    postDiv.find(".edit-form").hide(); // Hide the edit form
+    postDiv.find(".content-display").show(); // Show the content display
+  });
 });
