c77b10a8b6998a321fa57fdac290c33ce4fdaddc
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-06-26 06:30:06 +0900

    jQueryインポート

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,23 +1,14 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import $ from 'jquery';
+window.$ = $;
+window.jQuery = $;
 
-require("@rails/ujs").start()
+import Rails from "@rails/ujs";
+Rails.start();
+
 // require("turbolinks").start()  // コメントアウトされています
-require("@rails/activestorage").start()
-require("channels")
-require("@rails/activestorage").start()
-require("packs/post_park_dropdown")
-//= require jquery
-//= require jquery_ujs
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+require("@rails/activestorage").start();
+require("channels");
+require("packs/post_park_dropdown");
 
 $.ajaxSetup({
   headers: {
