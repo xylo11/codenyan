a33264e4cd30772e2cc5bd6bf49135bad8bed724
xylo11 <132252294+xylo11@users.noreply.github.com>
2023-07-04 18:27:19 +0900

    一覧の表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,10 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
+  def index
+    @posts = Post.all
+  end
+
   def new
     @post = Post.new
   end
